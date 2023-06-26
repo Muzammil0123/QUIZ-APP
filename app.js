dd4b41a16bf97e46dd146a6a3712bec217f7591d
Muzammil0123 <110159346+Muzammil0123@users.noreply.github.com>
2023-06-26 13:54:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-let a=1;
-dow.addEventListener("click",()=>{
-    if(a){
-    drop.style.top="50px";
-    a--;
-    }
-    else{
-        drop.style.top="-100px";
-        a++;  
-    }
 
-})
 main.getElementsByTagName("button")[0].addEventListener("click",()=>{
-    window.location="./categories.html";
+    window.location="./question.html";
+    console.log("hi");
 })
 
 
